d4328036f6ad4cff38c234878051d8b9805eab9f
Joshua Sanders <ninjosh2@gmail.com>
2019-12-15 20:13:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@ def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
       
+<<<<<<< HEAD
       elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
       
@@ -10,3 +11,9 @@ def speak_to_grandma(phrase)
       
     end
   end
+=======
+    else 
+      returns "HUH?! "
+
+
+>>>>>>> 690e5a00b1a13bfe3772fc3183aa114f0936808e
